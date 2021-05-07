14af3d3c68935c30d25326f12551fb1405127caf
unknown <nihaljha33@gmail.com>
2021-05-07 16:33:35 +0530

    practice code added

---
+++ C++ tutorial/DataType.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 20;
+    cout<<"size of int: "<<sizeof(a)<<endl;
+
+    float b = 12;
+    cout<<"value of b: "<<b<<". size of float: "<<sizeof(b)<<endl;
+
+    char c = 'a';
+    cout<<"value of c: "<<c<<". size of char: "<<sizeof(c)<<endl;
+
+    double d = 120;
+    cout<<"value of d: "<<d<<". Size of doule: "<<sizeof(d)<<endl;
+
+    return 0; 
+} 
